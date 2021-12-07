6ca0a0aaca06905032e6e54503577d8d5a97f2b8
Devorris Russell <devorris_r@yahoo.com>
2021-12-06 21:17:48 -0500

    adding post

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const Post = require("../models/Post")
 
 
 router.post("/", async (req,res)=>{
@@ -12,5 +12,18 @@ router.post("/", async (req,res)=>{
         res.status(500).json(err);
     }
 });
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+        } else {
+            res.status(403).json("you can update only your post")
+        }
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
